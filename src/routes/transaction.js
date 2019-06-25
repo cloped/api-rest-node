5a461ca11beb424335c698bf1c0e78ca36b4997d
Eduardo Lopes <educarvalholopes98@gmail.com>
2019-06-25 05:14:06 -0400

    fix

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -5,5 +5,6 @@ module.exports = function (app) {
   app.get('/transactions', TransactionController.readTransactions);
   app.get('/transactions/:userId', TransactionController.readUserTransactions);
   app.post('/transactions/transaction/:providerId/:recipientId', TransactionController.createTransactionRedirect);
+  app.get('/transactions/transaction/:providerId/:recipientId', TransactionController.createTransactionRedirect);
   app.post('/transactions/payment/:userId', PaymentController.createPayment);
 }
